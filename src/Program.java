7b5e87ed3ee511fe08f5f4d843368f0a21f2a817
robvat <rob.vattoy@outlook.com>
2015-03-25 11:09:06 +0100

    Main function implemented

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Robin on 25.03.2015.
+ */
+public class Program
+{
+    public static void main(String[] args)
+    {
+        Roundabout roundabout = new Roundabout();
+    }
+}
---
+++ src/Roundabout.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Robin on 25.03.2015.
+ */
+public class Roundabout
+{
+
+}
