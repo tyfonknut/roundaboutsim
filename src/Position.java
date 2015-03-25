fc085883879c4169e2ce92f8c4514a693daf814e
robvat <rob.vattoy@outlook.com>
2015-03-25 11:18:31 +0100

    Created roundabout array and added position

---
+++ src/Position.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Robin on 25.03.2015.
+ */
+public class Position
+{
+    boolean ocupied;
+    public Position()
+    {
+        ocupied = false;
+    }
+}
--- src/Roundabout.java
+++ src/Roundabout.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 /**
  * Created by Robin on 25.03.2015.
  */
 public class Roundabout
 {
+    int roundaboutSize;
+    int speedLimit;
 
+    public Roundabout()
+    {
+        ArrayList<Position> roundabout = new ArrayList();
+    }
 }
